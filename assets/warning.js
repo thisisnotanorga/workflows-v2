39bfdc81affe2b0ca5b9f0d0af9a450c50576e16
Douxx <douxx@douxx.tech>
2025-03-12 13:50:51 +0100

    Delete assets/warning.js

--- assets/warning.js
+++
@@ -1,8 +0,0 @@
-//Warning.js | Shows 20 Warnings in the client console
-
-for (let i = 0; i < 20; i++) {
-    console.log('%cHold up!', 'color: #5865f2; font-size: 100px; font-weight: bold; text-shadow: 2px 2px black;');
-    console.log('%cInvaders must die. Go away!', 'color: red; font-style: italic; font-size: 20px;');
-    console.log('%cIf you need any help, Contact @douxx.xyz on discord or send a mail to douxx@douxx.tech!', 'color: white; font-size: 18px;');
-}
-log('Warnings sent!', 'success');
