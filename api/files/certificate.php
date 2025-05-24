bd4d5a3724b632cbc18fea48c90f3cb4e1660498
douxxtech <douxx@douxx.tech>
2025-05-24 21:05:12 +0200

    only noskid.today allowed in cors

--- api/files/certificate.php
+++ api/files/certificate.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: noskid.today');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
